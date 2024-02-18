9e2d652524355798674fc4efb4a2565cd9f009d2
omri.gozlan <omri.gozlan@post.runi.ac.il>
2024-02-18 23:37:33 +0200

    HW9OmriGozlanUpdate5

--- List.java
+++ List.java
@@ -42,7 +42,7 @@ public class List {
        String ans = "";
        while (current != null){
             ans += current.cp.toString();
-            System.out.println(current.cp.toString());
+            System.out.print(current.cp.toString());
             current = current.next;
         }
         return ans;
